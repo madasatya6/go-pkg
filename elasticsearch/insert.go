de4a34e3a7d0e4721b68403e5575bfb1c2a146c4
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-12-19 08:08:35 +0700

    up

---
+++ elasticsearch/insert.go
@@ -0,0 +1,40 @@
+package elasticsearch
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/******
+* @author madasatya6
+* insert data
+*/
+func (s *Elastic) Insert(data map[string]interface{}) error {
+	var response string
+	dataJson, err := json.Marshal(data))
+	if err != nil {
+		return err 
+	}
+
+	bodyJson := string(dataJson)
+
+	if s.TypeName != "" {
+		response, err = s.Conn.Index().
+			Index(s.IndexName).
+			Type(s.TypeName).
+			BodyJson(bodyJson).
+			Do(s.Ctx)
+	} else {
+		response, err = s.Conn.Index().
+			Index(s.IndexName).
+			BodyJson(bodyJson).
+			Do(s.Ctx)
+	}
+
+	if err != nil {
+		return err 
+	}
+
+	fmt.Println("[Elastic][InsertProduct]Insertion Successful response: ", response)
+	return nil
+}
