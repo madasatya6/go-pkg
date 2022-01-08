10c5fd3cab37c7ea3cf9be463e4318af4ad1c928
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2022-01-08 20:36:13 +0700

    up

--- go.mod
+++ go.mod
@@ -9,29 +9,22 @@ require (
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/websocket v1.4.2
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/novalagung/gubrak/v2 v2.0.1
 	github.com/olivere/elastic/v7 v7.0.30
+	go.uber.org/zap v1.20.0
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
-	golang.org/x/text v0.3.6 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ uberzap/uberzap.go
@@ -0,0 +1,57 @@
+package uberzap
+
+import (
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// reference uberzap: https://medium.com/@gustavo.nabakseixas/go-using-uber-zap-in-your-application-135756f23bdc
+func createDirectoryIfNotExists() {
+	// create log directory
+	path, _ := os.Getwd()
+	if _, err := os.Stat(fmt.Sprintf("%s/logs", path)); os.IsNotExist(err) {
+		_ = os.Mkdir("logs", os.ModePerm)
+	}
+}
+
+func getLogWriter(filename string) zapcore.WriteSyncer {
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.OpenFile(path + "/logs/" + filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	return zapcore.AddSync(file)
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.UTC().Format("2006-01-02T15:04:05Z0700"))
+	})
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func InitLogger(filename string) {
+	createDirectoryIfNotExists()
+	writerSync := getLogWriter(filename)
+	encoder := getEncoder()
+
+	core := zapcore.NewCore(encoder, writerSync, zapcore.DebugLevel)
+	logg := zap.New(core, zap.AddCaller())
+
+	zap.ReplaceGlobals(logg)
+}
+
+/*
+func main(){
+	fmt.Println("Implementasi uber zap logging")
+	InitLogger("januari-2022.txt")
+	zap.S().Info("Information debug uber zap")
+}*/
